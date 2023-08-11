7ef8d448b7419696e63085c9acbdac59b5476134
ndtsman <alanliuyc@gmail.com>
2023-08-11 22:07:48 +1000

    operations with dates

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-08-06T17:01:17.194Z',
+    '2023-08-10T23:36:17.929Z',
+    '2023-08-11T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -36,10 +36,10 @@ const account2 = {
     '2019-11-30T09:48:16.867Z',
     '2019-12-25T06:04:23.907Z',
     '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2023-07-05T16:33:06.386Z',
+    '2023-08-05T14:43:26.374Z',
+    '2023-08-10T18:49:59.371Z',
+    '2023-08-11T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -74,6 +74,32 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//functions
+
+//put as an outside function
+const formatDate = (date) => {
+  // console.log(date);
+  const calcDaysPassed = (date1, date2) =>
+    //must round to get correct
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  //here can pass the second variable directly in a formated new date() style
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+  if (daysPassed === 0) return `Today`;
+  if (daysPassed === 1) return `Yesterday`;
+  if (daysPassed <= 7 && daysPassed > 1) return `${daysPassed} days ago`;
+  else {
+    //need to call: ie. getdate()
+    const day = String(date.getDate()).padStart(2, 0);
+    const month = String(date.getMonth() + 1).padStart(2, 0);
+    const year = String(date.getFullYear()).padStart(2, 0);
+
+    return `${day}/${month}/${year}`;
+  }
+  // const newDate = new Date(date);
+};
+
 const disPlayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
   // console.log(movement);
@@ -83,11 +109,12 @@ const disPlayMovements = function (acc, sort = false) {
     : acc.movements;
 
   movs.forEach(function (mov, i) {
-    const newDate = new Date(acc.movementsDates[i]);
-    const year = String(newDate.getFullYear()).padStart(2, 0);
-    const month = String(newDate.getMonth() + 1).padStart(2, 0);
-    const date = String(newDate.getDate()).padStart(2, 0);
-    const displayDate = `${date}/${month}/${year}`;
+    //must pass in formated date not acc.movementsDates[i]directly
+    const date = new Date(acc.movementsDates[i]);
+
+    //one variable is easier
+    const displayDate = formatDate(date);
+    // console.log(displayDate);
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class='movements__row'>
